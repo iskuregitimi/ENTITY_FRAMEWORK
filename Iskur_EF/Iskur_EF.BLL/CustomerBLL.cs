49ba9e9652ed41bd51ce6eca92eadebd67477ad5
Mehmet Fatih AKAN <akanmf@yahoo.com>
2019-04-15 14:06:26 +0300

    customerBLL oluşturuldu

---
+++ Iskur_EF/Iskur_EF.BLL/CustomerBLL.cs
@@ -0,0 +1,29 @@
+﻿using Iskur_EF.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iskur_EF.BLL
+{
+    public static class CustomerBLL
+    {
+        public static object GetCustomers(string searchText)
+        {
+            AdventureWorksEntities dataContext = new AdventureWorksEntities();
+
+            var result = dataContext.Customers.Where(c => c.PersonID != null).Select(
+                x => new
+                {
+                    x.CustomerID,
+                    x.Person.FirstName,
+                    x.Person.LastName,
+                    x.TerritoryID
+                }
+                );
+
+            return result.ToList();
+        }
+    }
+}

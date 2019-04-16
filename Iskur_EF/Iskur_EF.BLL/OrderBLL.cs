b1156a0243650b6382d5bd5f4b0ac62a154d9416
büsra başaran <busrab92@hotmail.com>
2019-04-16 14:51:47 +0300

    kredi karı ıd si girildi

---
+++ Iskur_EF/Iskur_EF.BLL/OrderBLL.cs
@@ -0,0 +1,48 @@
+﻿using Iskur_EF.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iskur_EF.BLL
+{
+    public static class OrderBLL
+    {
+        public static object GetProducts()
+        {
+            AdventureWorksEntities dataContext = new AdventureWorksEntities();
+            var result=dataContext.Products.Where(p=> p.ProductID>705).Select(
+                x => new
+                {
+                    x.ProductID,
+                    x.Name,
+                    x.StandardCost,
+                    x.ListPrice
+                }
+                );
+            return result.ToList();
+        }
+        public static List<Person> GetSalesPerson()
+        {
+            AdventureWorksEntities dataContext = new AdventureWorksEntities();
+            var result = dataContext.People.Where(p => p.BusinessEntityID > 273 && p.BusinessEntityID < 291);
+            return result.ToList();
+
+            //object metod ile
+            //var result = dataContext.People.Where(p => p.BusinessEntityID > 273 && p.BusinessEntityID < 291).Select(
+            //    x => new
+            //    {
+            //        x.FirstName
+            //    }
+            //    );
+            //return result.ToArray();
+        }
+        public static List<CreditCard> GetCreditCardID()
+        {
+            AdventureWorksEntities dataContext = new AdventureWorksEntities();
+            var result = dataContext.CreditCards;
+            return result.ToList();
+        }
+    }
+}
--- Iskur_EF/Iskur_EF.UI.Win/frm_YeniSiparis.cs
+++ Iskur_EF/Iskur_EF.UI.Win/frm_YeniSiparis.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Iskur_EF.BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,6 +26,22 @@ namespace Iskur_EF.UI.Win
         private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void frm_YeniSiparis_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = OrderBLL.GetProducts();
+
+            foreach (var item in OrderBLL.GetSalesPerson())
+            {
+                cmb_PersonelAdı.Items.Add(item.FirstName);
+            }
+            //cmb_PersonelAdı.DataSource = OrderBLL.GetSalesPerson();
+
+            foreach (var item in OrderBLL.GetCreditCardID())
+            {
+                cmb_KrediKartID.Items.Add(item.CreditCardID);
+            }
         }
     }
 }

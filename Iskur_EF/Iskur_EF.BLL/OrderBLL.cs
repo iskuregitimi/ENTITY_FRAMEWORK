0b67ba9621437ecafb794d21bd330a23cbba6fde
ismail.isik <ismail.isik@BK-LAB204-IS002>
2019-04-16 14:51:46 +0300

    KrediKartıIdsi Girildi

---
+++ Iskur_EF/Iskur_EF.BLL/OrderBLL.cs
@@ -0,0 +1,40 @@
+﻿using Iskur_EF.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iskur_EF.BLL
+{
+    public static class OrderBLL
+    {
+        public static object GetProduct()
+        {
+            AdventureWorksEntities db = new AdventureWorksEntities();
+            var a = db.Products.Where(x => x.ProductID > 705).Select(x => new
+            {
+                x.ProductID,
+                x.Name,
+                x.StandardCost,
+                x.ListPrice
+
+            });
+            return a.ToList();
+        }
+
+        public static List<Person> Get_SalePerson()
+        {
+            AdventureWorksEntities db = new AdventureWorksEntities();
+            var a = db.People.Where(x => x.BusinessEntityID > 273 && x.BusinessEntityID <= 291);
+            return a.ToList();
+        }
+        public static List<CreditCard> GetCreditCardsID()
+        {
+            AdventureWorksEntities db = new AdventureWorksEntities();
+            var a = db.CreditCards;
+            return a.ToList();
+        }
+
+    }
+}
--- Iskur_EF/Iskur_EF.UI.Win/frm_YeniSiparis.cs
+++ Iskur_EF/Iskur_EF.UI.Win/frm_YeniSiparis.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Iskur_EF.BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,6 +26,19 @@ namespace Iskur_EF.UI.Win
         private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void frm_YeniSiparis_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource= OrderBLL.GetProduct();
+            foreach (var item in OrderBLL.Get_SalePerson())
+            {
+                cmbSatısPersoneli.Items.Add(item.FirstName);
+            }
+            foreach (var item in OrderBLL.GetCreditCardsID())
+            {
+                cmbID.Items.Add(item.CreditCardID);
+            }
         }
     }
 }

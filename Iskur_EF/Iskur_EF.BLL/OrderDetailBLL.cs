79fb873c205071f3ed6850680ac0f66866585a49
fadile.avci <fadile.avci@BK-LAB204-IS026>
2019-04-16 12:25:13 +0300

    Commit2

--- Iskur_EF/Iskur_EF.BLL/OrderDetailBLL.cs
+++ Iskur_EF/Iskur_EF.BLL/OrderDetailBLL.cs
@@ -8,10 +8,36 @@ using Iskur_EF.DAL;
 namespace Iskur_EF.BLL
 {
     public class OrderDetailBLL
+
+
+
     {
-        public static void AddOrderDetails(Product product, object header)
+
+        static AdventureWorksEntities dataContext = new AdventureWorksEntities();
+
+        public static List<SalesOrderDetail> GetOrderDetails(int id)
         {
-           
+            return dataContext.SalesOrderDetails.Where(x => x.SalesOrderID == id).ToList();
+        }
+
+        public static void AddOrderDetails(Product product, SalesOrderHeader orderHeader)
+        {
+      
+                SalesOrderDetail orderDetail = new SalesOrderDetail();
+                orderDetail.SalesOrderID = orderHeader.SalesOrderID;
+                orderDetail.ProductID = product.ProductID;
+                orderDetail.SpecialOfferID = product.SpecialOfferProducts.FirstOrDefault(x => x.ProductID ==product.ProductID).SpecialOfferID;
+                orderDetail.OrderQty = 1;
+                orderDetail.UnitPrice = product.ProductCostHistories.FirstOrDefault(x => x.ProductID ==product.ProductID).StandardCost;
+                orderDetail.UnitPriceDiscount = 0;
+                orderDetail.rowguid = Guid.NewGuid();
+                orderDetail.ModifiedDate = Convert.ToDateTime(DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss"));
+
+                dataContext.SalesOrderDetails.Add(orderDetail);
+                dataContext.SaveChanges();
+            }
+
+
         }
     }
 }
